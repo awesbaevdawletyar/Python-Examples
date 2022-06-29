fedfe4f00b93868971da59153953a9797cb90a00
awesbaevdawletyar <awesbaevdawletyar@gmail.com>
2022-06-29 02:55:42 -0700

    python examples

---
+++ Begiin1-40/# if opertori.py
@@ -0,0 +1,7 @@
+# if opertori 
+a=2
+if a%2==0:
+    print("jup san")
+else:
+    print("taq san")
+
---
+++ Begiin1-40/Ulken kishi.py
@@ -0,0 +1,9 @@
+a=int(input('a='))
+b=int(input('b='))
+
+if a>b:
+    print("a ulken")
+elif a==b:
+    print("eki sanda ten'")
+else:
+    print("b sani ulken")
